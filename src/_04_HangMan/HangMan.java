87bde4af460adcc5d05e97cc4a291b9b767df6cb
League <League@administorsimac.attlocal.net>
2018-10-13 17:27:21 -0700

    Module 1(IP)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -20,6 +20,8 @@ public class HangMan implements KeyListener {
 	boolean tempWin = false;
 	boolean officialWin;
 	String popped;
+	int num;
+	boolean permWin = false;
 
 	public static void main(String[] args) {
 		HangMan run = new HangMan();
@@ -32,7 +34,7 @@ public class HangMan implements KeyListener {
 		frame.setVisible(true);
 		frame.addKeyListener(this);
 		frame.pack();
-		int num = Integer.parseInt(JOptionPane.showInputDialog("How many words would you like to guess?"));
+		num = Integer.parseInt(JOptionPane.showInputDialog("How many words would you like to guess?"));
 		for (int i = 0; i < num; i++) {
 			String word = a.readRandomLineFromFile("src/_04_HangMan/dictionary.txt");
 			w = word;
@@ -41,6 +43,7 @@ public class HangMan implements KeyListener {
 			} else {
 
 				stack.push(word);
+				System.out.println(word);
 			}
 		}
 		popped = stack.pop();
@@ -72,75 +75,103 @@ public class HangMan implements KeyListener {
 		String prev = "";
 		String part1;
 		String part2;
-		String done = "";
+		String done = label.getText();
 		if (w.contains(s)) {
-			int index = w.indexOf(user);
-			for (int i = 0; i < w.length(); i++) {
-				if (user == w.charAt(i)) {
+			for (int i = 0; i < popped.length(); i++) {
+				if (user == popped.charAt(i)) {
 					prev = label.getText();
-					part1 = prev.substring(0, i - 1);
-					part2 = prev.substring(i + 1, w.length() - 1);
-					label.setText(part1 + w + part2);
+					part1 = prev.substring(0, i);
+					if (i + 1 == popped.length()) {
+						part2 = "";
+					} 
+					
+					else {
+						part2 = prev.substring(i + 1, w.length());
+					}
+					label.setText(part1 + s + part2);
 					done = label.getText();
+					System.out.println(w);
 				}
 			}
 		}
-				
+
 		else {
 			lives = lives - 1;
 		}
-		
-		if(done.contains("_")){
+
+		if (done.contains("_")) {
 			tempWin = false;
 		}
-		
+
 		else {
 			tempWin = true;
 		}
-		
-		if(tempWin == true) {
+
+		if (tempWin == true) {
+			num--;
+			if (num == 0) {
+				permWin = true;
+				JOptionPane.showMessageDialog(null, "You Won!!!!!!");
+				int result = JOptionPane.showConfirmDialog(null, "Do you want to play again?", null,
+						JOptionPane.YES_NO_OPTION);
+				if (result == JOptionPane.YES_OPTION) {
+					reset();
+				} else {
+					System.exit(0);
+				}
+			}
+		}
+
+		if (tempWin == true && stack.isEmpty() == false) {
+			label.setText("");
 			popped = stack.pop();
 			int length = popped.length();
 			label.setText("_");
 			for (int i = 0; i < length - 1; i++) {
 				label.setText(label.getText() + "_");
 				frame.pack();
-				tempWin = false;
 			}
+
+			lives = 9;
 		}
-		if(lives == 0) {
+
+		if (lives == 0) {
 			JOptionPane.showMessageDialog(null, "You Lost!!!");
-			int result = JOptionPane.showConfirmDialog(null, 
-					   "Do you want to play again?",null, JOptionPane.YES_NO_OPTION);
-					if(result == JOptionPane.YES_OPTION) {
-					    reset();
-					} 
-					else {
-						System.exit(0);
-					}
+			int result = JOptionPane.showConfirmDialog(null, "Do you want to play again?", null,
+					JOptionPane.YES_NO_OPTION);
+			if (result == JOptionPane.YES_OPTION) {
+				reset();
+			} else {
+				System.exit(0);
+			}
 
 		}
-		
-		if(tempWin == true && stack.isEmpty()) {
-			JOptionPane.showMessageDialog(null, "You Won!!!!!!");
-			int result = JOptionPane.showConfirmDialog(null, 
-					   "Do you want to play again?",null, JOptionPane.YES_NO_OPTION);
-					if(result == JOptionPane.YES_OPTION) {
-					    reset();
-					} 
-					else {
-						System.exit(0);
-					}
 
-		}
-		
 	}
 
 	public void reset() {
-		System.exit(0);
-		HangMan run = new HangMan();
-		run.MakeStuff();
+		label.setText("");
+		num = Integer.parseInt(JOptionPane.showInputDialog("How many words would you like to guess?"));
+		for (int i = 0; i < num; i++) {
+			String word = a.readRandomLineFromFile("src/_04_HangMan/dictionary.txt");
+			w = word;
+			if (stack.contains(word)) {
+				i = i - 1;
+			} else {
+
+				stack.push(word);
+			}
+		}
+		popped = stack.pop();
+		int length = popped.length();
+		label.setText("_");
+		for (int i = 0; i < length - 1; i++) {
+			label.setText(label.getText() + "_");
+			frame.pack();
+			lives = 9;
+		}
 	}
+
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
